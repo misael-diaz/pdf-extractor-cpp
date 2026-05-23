f27c5c2a31a8f8327bc5bd4f5d9f46dd8671f889
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-05-23 19:09:33 -0400

    calls madvise for performance

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 #include <poppler-document.h>
 #include <poppler-page.h>
 #include <sys/mman.h>
@@ -10,6 +13,7 @@
 
 int main()
 {
+	int64_t rc = 0;
 	uint64_t const pagesz = sysconf(_SC_PAGESIZE);
 	uint64_t len_mmap = (pagesz << 1);
 
@@ -24,12 +28,23 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
+	rc = madvise(buf, len_mmap, MADV_WILLNEED);
+	if (-1 == rc) {
+		fprintf(stderr, "%s", "error: madvise fast access failed\n");
+		if (errno) {
+			fprintf(stderr, "%s\n", strerror(errno));
+		}
+		exit(EXIT_FAILURE);
+	}
+
+
 	std::unique_ptr<poppler::document> doc(poppler::document::load_from_file("doc.pdf"));
 	if (!doc) {
 		fprintf(stderr, "%s", "error: failed to load document from file\n");
 		exit(EXIT_FAILURE);
 	}
 
+	// NOTE: we are assuming that the important is on the first page
 	std::unique_ptr<poppler::page> page(doc->create_page(0));
 	if (!page) {
 		fprintf(stderr, "%s", "error: failed to create page\n");
